2ecec2b4811f08afb3ca46ec8d1243c105c48bd7
Adam Caron <adamwcaron@gmail.com>
2015-07-02 12:39:48 -0600

    adds task index and 'new task' routes

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,19 @@ class TaskManagerApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
 
   get '/' do
-    'hellp, world!'
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = ["task1", "task2", "task3"]
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
   end
 end
