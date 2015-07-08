6cb2ebec5f89a6e6c50155ee6b6239db60260809
Adam Caron <adamwcaron@gmail.com>
2015-07-08 09:42:10 -0600

    removes unnecessary requirements from controller and model

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,8 +1,4 @@
-require 'models/task_manager'
-
 class TaskManagerApp < Sinatra::Base
-  set :root, File.join(File.dirname(__FILE__), '..')
-  set :method_override, true  # this allows us to use _method in the form
 
   get '/' do
     erb :dashboard
