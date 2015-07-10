2b772370e0724e838e1cdfc5afe22ae84cf88231
Adam Caron <adamwcaron@gmail.com>
2015-07-10 15:42:31 -0600

    adds explanation for loop

--- db/migrations/001_create_tasks.rb
+++ db/migrations/001_create_tasks.rb
@@ -1,3 +1,4 @@
+
 require 'sequel'
 require 'sqlite3'
 require './app/models/task_manager'
@@ -5,9 +6,15 @@ environments = %w(test development)
 
 environments.each do |env|
   Sequel.sqlite("db/task_manager_#{env}.sqlite3").create_table :tasks do
-    primary_key :id
-    String      :title
-    String      :description
+    primary_key   :id
+    String        :title
+    String        :description
   end
   puts "creating tasks table for #{env}"
 end
+
+# only way to hit both environments in one call is to loop
+# through and just change the file names
+# otherwise we are only getting one of them and would have to do two migrations
+
+# important to understand that the test and the development environment are independent
