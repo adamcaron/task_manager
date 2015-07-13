5d98094b537cd0856e6b16460409e11f52f1d3d8
Adam Caron <adamwcaron@gmail.com>
2015-07-12 19:14:39 -0500

    refactors 'create' test so it calls the id dynamically instead of manually

--- test/models/task_manager_test.rb
+++ test/models/task_manager_test.rb
@@ -2,18 +2,16 @@ require_relative '../test_helper'
 
 class TaskManagerTest < Minitest::Test
   def test_it_creates_a_task
-    TaskManager.create({ :title       => "a title",
-                         :description => "a description" })
-    task = TaskManager.find(1)
+    TaskManager.create({ :title => "a title", :description => "a description" })
+    task = TaskManager.find(TaskManager.all.first.id)
     assert_equal "a title", task.title
     assert_equal "a description", task.description
-    assert_equal 1, task.id
+    assert_equal TaskManager.all.first.id, task.id
   end
 
   def test_it_finds_all_tasks
     3.times do
-      TaskManager.create({ :title => "a title",
-                           :description => "a description"})
+      TaskManager.create({ :title => "a title", :description => "a description"})
     end
 
     assert_equal 3, TaskManager.all.count
@@ -22,7 +20,6 @@ class TaskManagerTest < Minitest::Test
   def test_it_finds_a_task_by_id
     TaskManager.create({ :title => "a title", :description => "a description" })
     TaskManager.create({ :title => "2nd title", :description => "2nd description" })
-
     assert_equal "2nd description", TaskManager.find(2).description
   end
 
